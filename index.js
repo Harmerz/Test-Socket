da2024ce58c5d181e114884ee7140b1b7a2eb891
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-26 19:35:03 +0700

    fix : cors origin *

--- index.js
+++ index.js
@@ -6,32 +6,17 @@ import { Server } from "socket.io";
 import cors from 'cors'
 
 const app = express();
+app.use(cors({ origin: "*" }));
 
 // get our port
 const port = process.env.PORT || 8080;
 
-var allowedOrigins = ['*']
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      // (like mobile apps or curl requests)
-      if (!origin) return callback(null, true)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          'The CORS policy for this site does not ' + 'allow access from the specified Origin.'
-        return callback(new Error(msg), false)
-      }
-      return callback(null, true)
-    },
-  })
-)
-
+// Allow requests from http://localhost:3000
 const server = http.createServer(app);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 function generateData() {
     const TEAM_ID = '1009';
     const now = new Date();
