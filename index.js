882da97850af925095bda3d9491f01e7b50be8e1
Haikal Hilmi <haikal6255hilmi@gmail.com>
2023-10-26 18:36:44 +0700

    fix : cors ES modules import

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import cors from 'cors'
 
 const app = express();
 const server = http.createServer(app);
 // get our port
 const port = process.env.PORT || 8080;
-const cors = require('cors')
 
 var allowedOrigins = ['*']
 app.use(
